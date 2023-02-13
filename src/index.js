8f5d04306fd0b4652ee9c82d1db3de8c87b72651
Florentin <florentin93600@gmail.com>
2023-02-13 09:01:39 +0100

    ReactDOM.render to ReactDOM.CreatRoot

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Home from './pages'
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route  } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ 
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+    <Router>
+        <Route path="/">
+            <Home />
+        </Route>
+        <Route path="/survey">
+            <Survey />
+        </Route>
+    </Router>
+</React.StrictMode>
+)
