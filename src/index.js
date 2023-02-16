5936c721a4a9f0915a1f4312f8d5765b9bcab3f4
Florentin <florentin93600@gmail.com>
2023-02-16 11:09:43 +0100

    router vers 404 OK

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom'
 import Home from './pages/Home'
 import Survey from './pages/Survey'
+import Header from './components/Header'
+import Error from './components/Error'
 console.log(React.version);
 
  
@@ -11,10 +13,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
     <Router>
-        <Routes>
-            <Route exact path="/" element={<Home />}/>
-            <Route path="/survey" element={<Survey />} />
-        </Routes>
+        <Header />
+            <Routes>
+                <Route exact path="/" element={<Home />}/>
+                <Route path="/survey/:questionNumber" element={<Survey />} />
+                <Route path="*" element={<Error/>}/> 
+            </Routes>
     </Router>
 </React.StrictMode>
 )
