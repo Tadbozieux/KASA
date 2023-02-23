d80ac229140e14bc678628357fb54cfacbc827a3
Florentin <florentin93600@gmail.com>
2023-02-23 13:01:36 +0100

    collapse fonctionnelle

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -1,58 +1,68 @@
-import { useParams } from 'react-router-dom'
+
 import "./Apropos.css"
 
-
-let coll = document.getElementsByClassName("collapsible");
-let i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 function Apropos() {
-    const { questionNumber } = useParams()
- 
-    return (
-    <div>  
-        <div>
-            <button type="button" class="collapsible">Fiabilité</button>
-            <div class="content">
-            <p>Lorem ipsum...</p>
-            </div>
-        </div>
-        <div>
-            <button type="button" class="collapsible">Respect</button>
-            <div class="content">
-            <p>Lorem ipsum...</p>
-            </div>
-        </div>
-        <div>
-            <button type="button" class="collapsible">Service</button>
-            <div class="content">
-            <p>Lorem ipsum...</p>
-            </div>
-        </div>
-        <div>
-            <button type="button" class="collapsible">Securite</button>
-            <div class="content">
-            <p>Lorem ipsum...</p>
-            </div>
-        </div>
+  const { questionNumber } = useParams();
+//Le code est encapsulé dans une fonction qui est appelée lorsque l'événement window.onload est déclenché
+// Cela garantit que le code ne s'exécute qu'après le chargement complet de la page.
+  useEffect(() => {
+    window.onload = function () {  
+      let coll = document.getElementsByClassName('collapsible');
+      let i;
 
-    </div>  
-    )
+      for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener('click', function () {
+          this.classList.toggle('active');
+          let content = this.nextElementSibling;
+          if (content.style.display === 'block') {
+            content.style.display = 'none';
+          } else {
+            content.style.display = 'block';
+          }
+        });
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <button type="button" className="collapsible">
+          Fiabilite
+        </button>
+        <div className="content">
+          <p>Lorem ipsum...</p>
+        </div>
+      </div>
+      <div>
+        <button type="button" className="collapsible">
+          Respect
+        </button>
+        <div className="content">
+          <p>Lorem ipsum...</p>
+        </div>
+      </div>
+      <div>
+        <button type="button" className="collapsible">
+          Service
+        </button>
+        <div className="content">
+          <p>Lorem ipsum...</p>
+        </div>
+      </div>
+      <div>
+        <button type="button" className="collapsible">
+          Securite
+        </button>
+        <div className="content">
+          <p>Lorem ipsum...</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-
-
-export default Apropos
+export default Apropos;
