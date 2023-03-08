deef92a6f643fdd4a6670869d0988f76c2dfd6a3
Florentin <florentin93600@gmail.com>
2023-03-08 16:53:41 +0100

    Chevron OK enfinnn

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,13 +1,15 @@
 import './Collapse.css'
 import '../../App.css';
 import { useParams } from 'react-router-dom';
+import React, { useState } from 'react';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'  // Importation Chevron FontAwesome
 import {faChevronUp} from '@fortawesome/free-solid-svg-icons';
-
+import {faChevronDown} from '@fortawesome/free-solid-svg-icons';
 
 function Collapse() {
     const { questionNumber } = useParams();
+    const [collapsed, setCollapsed] = useState(false);
     
     // Cette fonction est appelée lorsque l'utilisateur clique sur un collapsible
   const handleCollapsibleClick = (e) => {
@@ -19,23 +21,24 @@ function Collapse() {
     const content = collapsible.nextElementSibling;
     // Change l'état d'affichage du contenu en fonction de son état actuel
     content.style.display = content.style.display === 'block' ? 'none' : 'block';
+    setCollapsed(!collapsed);
   }
   
     return (
       <div className='totalCollapsible'>
         <div className='blocCollapsible'>
-          <div type="button" className="collapsible" onClick={handleCollapsibleClick}>
-            <h2>Fiabilite</h2>
-            <FontAwesomeIcon icon={faChevronUp} />
-          </div>
-          <div className="content">
-            <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
-          </div>
-        </div>
+                <div type="button" className="collapsible" onClick={handleCollapsibleClick}>
+                    <h2>Fiabilite</h2>
+                    <FontAwesomeIcon icon={collapsed ? faChevronDown : faChevronUp} />
+                </div>
+                <div className="content">
+                    <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
+                </div>
+            </div>
         <div className='blocCollapsible'>
           <div type="button" className="collapsible" onClick={handleCollapsibleClick}>
             <h2>Respect</h2>
-            <FontAwesomeIcon icon={faChevronUp} />
+            <FontAwesomeIcon icon={collapsed ? faChevronDown : faChevronUp} />
           </div>
           <div className="content">
             <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
@@ -44,7 +47,7 @@ function Collapse() {
         <div className='blocCollapsible'>
           <div type="button" className="collapsible" onClick={handleCollapsibleClick}>
             <h2>Service</h2>
-            <FontAwesomeIcon icon={faChevronUp} />
+            <FontAwesomeIcon icon={collapsed ? faChevronDown : faChevronUp} />
           </div>
           <div className="content">
             <p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>
@@ -53,7 +56,7 @@ function Collapse() {
         <div className='blocCollapsible'>
           <div type="button" className="collapsible" onClick={handleCollapsibleClick}>
             <h2>Sécurité</h2>
-            <FontAwesomeIcon icon={faChevronUp} />
+            <FontAwesomeIcon icon={collapsed ? faChevronDown : faChevronUp} />
           </div>
           <div className="content">
             <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
