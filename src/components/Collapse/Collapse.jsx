c3d7d072cf073ac4cbfe48f6a5dcb1159db63eab
Florentin <florentin93600@gmail.com>
2023-04-25 11:43:47 +0200

    travail collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -8,7 +8,7 @@ import VectorDown from '../../assets/VectorDown.svg'
 
 
 
-function Collapse() {
+function Collapse({ collapsibleItems  }) {
     const [collapsed, setCollapsed] = useState(Array(4).fill(false));
   
     const handleCollapsibleClick = (index) => {
@@ -17,32 +17,11 @@ function Collapse() {
       setCollapsed(newCollapsed);
     };
   
-    const collapsibleContent = [     //creation de l'objet afin d'utiliser .map plutot que X4 les collapses
-      {
-        title: "Fiabilité",
-        content:
-          "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
-      },
-      {
-        title: "Respect",
-        content:
-          "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
-      },
-      {
-        title: "Service",
-        content:
-          "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
-      },
-      {
-        title: "Sécurité",
-        content:
-          "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
-      },
-    ];
+
   
     return (       // pour chaque collapse creation d'un bloc
       <div className="totalCollapsible">
-        {collapsibleContent.map((item, index) => (
+        {collapsibleItems .map((item, index) => (
           <div className="blocCollapsible" key={index}>
             <div
               type="button"
--- src/pages/Maison/Maison.jsx
+++ src/pages/Maison/Maison.jsx
@@ -1,77 +1,92 @@
-import React from "react"
+import React from "react";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react"
-import "./Maison.css"
-import datas from '../../data/data'
-import Carrousel from "../../components/Carrousel/Carrousel"
-import Footer from '../../components/footer/Footer';
-import CollapseMaison from "../../components/CollapseMaison/CollapseMaison"
-import redstar from '../../assets/redstar.svg'
-import greystar from '../../assets/greystar.svg'
+import { useEffect, useState } from "react";
+import "./Maison.css";
+import datas from "../../data/data";
+import Carrousel from "../../components/Carrousel/Carrousel";
+import Footer from "../../components/footer/Footer";
+import Collapse from "../../components/Collapse/Collapse";
+import redstar from "../../assets/redstar.svg";
+import greystar from "../../assets/greystar.svg";
 
 export default function Accomodation() {
-	
-	const [imageSlider, setImageSlider] = useState([]);             //Hook permettant de preserver données
+  const [imageSlider, setImageSlider] = useState([]);
 
-	const idAccomodation = useParams('id').id;
-	const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
-	
-	useEffect(() => {
-		const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
-		setImageSlider(dataCurrentAccomodation[0].pictures);
-	}, [idAccomodation]);
+  const idAccomodation = useParams("id").id;
+  const dataCurrentAccomodation = datas.filter(
+    (data) => data.id === idAccomodation
+  );
 
-	const name = dataCurrentAccomodation[0].host.name.split(' '); 
-	const rating = dataCurrentAccomodation[0].rating;
-	const description  = dataCurrentAccomodation[0].description;
-	const equipments = dataCurrentAccomodation[0].equipments;
+  useEffect(() => {
+    const dataCurrentAccomodation = datas.filter(
+      (data) => data.id === idAccomodation
+    );
+    setImageSlider(dataCurrentAccomodation[0].pictures);
+  }, [idAccomodation]);
 
-	return (
-		<>
-			
-			<Carrousel imageSlider={imageSlider}/>
-			<main className="accomodation">
-				<div className="accomodation_content">
-					<div className="accomodation_content_infos">
-						<h1>{dataCurrentAccomodation[0].title}</h1>
-						<p>{dataCurrentAccomodation[0].location}</p>
-						<div className="accomodation_content_infos_button">
-							{dataCurrentAccomodation[0].tags.map((tag, index) => {
-								return (
-									<button key={index}>{tag}</button>
-								)
-							})}
-						</div>
-					</div>
-					<div className="accomodation_content_host">
-						<div className="accomodation_content_hostnameimg">
-							<div className='accomodation_content_host_name'>
-								<p>{name[0]}</p>
-								<p>{name[1]}</p>
-							</div>
-							<img className='img_host'   src={dataCurrentAccomodation[0].host.picture} alt="host of this accomodation" />
-						</div>
-							
-						<div className="accomodation_content_host_stars">
-							{[...Array(5)].map((star, index) => {
-								const ratingValue = index + 1;
-								return (
-									<img key={index} src={ratingValue <= rating ? redstar : greystar} alt="star" />
-								)
-							})}
-						</div>
-					</div>
-				</div>
-				<div className="houseCollapses">
-            <div className="houseDetails">
-              <CollapseMaison title="Description" content={description} />
-            </div>
-            <div className="houseDetails">
-              <CollapseMaison title="Équipements" content={equipments} />
+  const name = dataCurrentAccomodation[0].host.name.split(" ");
+  const rating = dataCurrentAccomodation[0].rating;
+  const description = dataCurrentAccomodation[0].description;
+  const equipments = dataCurrentAccomodation[0].equipments;
+
+  const collapsibleContentMaison = [
+    {
+      title: "Description",
+      content: description,
+    },
+    {
+      title: "Équipements",
+      content: equipments.join(", "),
+    },
+  ];
+
+  return (
+    <>
+      <Carrousel imageSlider={imageSlider} />
+      <main className="accomodation">
+        <div className="accomodation_content">
+          <div className="accomodation_content_infos">
+            <h1>{dataCurrentAccomodation[0].title}</h1>
+            <p>{dataCurrentAccomodation[0].location}</p>
+            <div className="accomodation_content_infos_button">
+              {dataCurrentAccomodation[0].tags.map((tag, index) => {
+                return <button key={index}>{tag}</button>;
+              })}
             </div>
           </div>
-			</main>
-			<Footer/>
-		</>
-	)
+          <div className="accomodation_content_host">
+            <div className="accomodation_content_hostnameimg">
+              <div className="accomodation_content_host_name">
+                <p>{name[0]}</p>
+                <p>{name[1]}</p>
+              </div>
+              <img
+                className="img_host"
+                src={dataCurrentAccomodation[0].host.picture}
+                alt="host of this accomodation"
+              />
+            </div>
+            <div className="accomodation_content_host_stars">
+              {[...Array(5)].map((star, index) => {
+                const ratingValue = index + 1;
+                return (
+                  <img
+                    key={index}
+                    src={ratingValue <= rating ? redstar : greystar}
+                    alt="star"
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="houseCollapses">
+          <div className="houseDetails">
+            <Collapse collapsibleItems={collapsibleContentMaison} />
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
