c6cb6c0e4e8bbbd3e6c7e8e60871f73b10383f3d
Florentin <florentin93600@gmail.com>
2023-04-27 11:14:58 +0200

    fin?

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,31 +1,21 @@
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
 import './Card.css'
 
-
-const CardImage = styled.img`
-   
-   
-    
-height: 250px;
-    object-fit: cover;
-`
- 
-function Card({  title, cover }) {
+function Card({ title, cover }) {
     return (
         <div className='Card'> 
-            <CardImage src={cover} alt="accomodation" />
+            <img className='CardImage' src={cover} alt="accomodation" />
             <span className='title'>{title}</span>
         </div>
     )
 }
- 
+
 Card.propTypes = {
     label: PropTypes.string,
     title: PropTypes.string.isRequired,
     picture: PropTypes.string,
 }
- 
+
 Card.defaultProps = {
     title: 'Mon titre par défaut',
 }
