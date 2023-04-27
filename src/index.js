93ab7fcbdb962d7c4491fcb01a8da6e6155c768e
Florentin <florentin93600@gmail.com>
2023-04-27 10:25:23 +0200

    supression dependances non demandées

--- src/index.js
+++ src/index.js
@@ -6,25 +6,17 @@ import Maison from './pages/Maison/Maison'
 import Apropos from './pages/Apropos/Apropos'
 import Header from './components/Header/Header'
 import Error from '../src/pages/Error/Error'
-import { createGlobalStyle } from 'styled-components'
 import './index.css'
 
 
 
 
-console.log(React.version);
 
-const GlobalStyle = createGlobalStyle`
-    div {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-`
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-    <Router basename="/">
-        <GlobalStyle/>
+    <Router >
         <Header />
             <Routes>
                 <Route path="/" element={<Home />}/>
